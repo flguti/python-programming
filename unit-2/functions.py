8d516eb9c114b927283b4ba967b26af003c67034
flavio lopes <flavio@Danielas-MacBook.local>
2019-03-11 20:40:44 -0400

    unit3

---
+++ unit-2/functions.py
@@ -0,0 +1,61 @@
+'''
+def high_low():
+    my_variable = 3
+    if my_variable > 10:
+        print('hight')
+    else :
+        print('low')
+'''
+
+'''
+def high_low(my_var):
+    if my_var > 10:
+        print('hight')
+    else :
+        print('low')
+
+
+high_low(5)
+high_low(15)
+
+
+
+def fizzbuzz():
+    for x in range(1, 102):
+        if(x%3 == 0 and x%5 == 0):
+            print('fizzbuzz')        
+        elif x%3 == 0:
+            print('fizz')
+        elif x%5 == 0:
+            print('buzz')
+        else:
+            print(x)
+
+fizzbuzz()
+
+
+
+
+def capitalize(my_name):
+    return my_name.upper()
+
+print(capitalize('flavio'))
+
+
+
+def add_two(number):
+    total = number + 2
+    print(total)
+    re
+
+add_two(4)
+
+
+'''
+def cap_first_letter(sentence)
+    table = [sentence.split('')]
+    for idx in range(len(table))
+        
+        
+    print(sentence.split(''))
+
