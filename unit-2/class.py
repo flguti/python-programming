644057e77744a196eead9aca2b5d8da36a406f41
flavio lopes <flavio@Danielas-MacBook.local>
2019-03-25 10:07:49 -0400

    unit-3

--- unit-2/class.py
+++ unit-2/class.py
@@ -22,6 +22,7 @@ student_2 = Student('marcus', 'Guelph')
 print(student_2.name)
 print(student_1.name_and_address())
 '''
+from random import randint
 
 class Playlist:
     def __init__(self):
@@ -38,13 +39,23 @@ class Playlist:
             print('{} - by {}'.format(song['tittle'], song['artist']))
     def size(self):
         print(len(my_songs.playlist))
+    def shuffle(self):
+        for idx, _ in enumerate(self.playlist):
+            x = randint(0, len(my_songs.playlist) - 1)
+            self.playlist[idx], self.playlist[x] = self.playlist[x], self.playlist[idx]
 
 
 
 my_songs = Playlist()
 my_songs.add_to_playlist({'id': 0,'tittle': 'i dont know', 'artist': 'Cardi B'})
 my_songs.add_to_playlist({'id': 1,'tittle': 'the flag song', 'artist': 'Unknow'})
+my_songs.add_to_playlist({'id': 2,'tittle': 'whatever', 'artist': 'Cardi B'})
+my_songs.add_to_playlist({'id': 3,'tittle': 'one more song', 'artist': 'Unknow'})
+my_songs.add_to_playlist({'id': 4,'tittle': 'another song', 'artist': 'Cardi B'})
+my_songs.add_to_playlist({'id': 5,'tittle': 'last song', 'artist': 'Unknow'})
 my_songs.get_songs()
-my_songs.remove_from_playlist(1)
-my_songs.get_songs()
+#my_songs.remove_from_playlist(1)
 my_songs.size()
+my_songs.shuffle()
+my_songs.get_songs()
+#my_songs.size()
